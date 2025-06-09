cd15aa69177eb3a49bc1901f4a906eae6aa0f706
Noah Hitz <94022923+NoHitzz@users.noreply.github.com>
2025-06-09 19:06:40 +0200

    fixed examples

--- src/example.java
+++ src/example.java
@@ -10,18 +10,18 @@ import com.emu.*;
 class example {
     static SwitchParam active = new SwitchParam("active", "a", false, "Switches mode to active");
     static StrParam inputFile = new StrParam("file", "f", "", "FILE", "Specify an input file");
-    static StrParam inputUrl = new StrParam("url", "u", "", "URL", "Specify an input url");
-    static BoolParam convert = new BoolParam("convert", "", false, "BOOL", "Enable/Disable number conversions");
-    static IntParam group = new IntParam("group", "", 1, "INT", "Number of group entries");
-    static StrParam output = new StrParam("out", "o", "", "FILE", "Specify an output file");
+    static StrParam inputUrl  = new StrParam("url", "u", "", "URL", "Specify an input url");
+    static BoolParam convert  = new BoolParam("convert", "", false, "BOOL", "Enable/Disable number conversions");
+    static IntParam group     = new IntParam("group", "", 1, "INT", "Number of group entries");
+    static StrParam output    = new StrParam("out", "o", "", "FILE", "Specify an output file");
 
     public static void main(String[] args) {
-        CommandParser commandParser = new CommandParser("java example.jar");
+        CommandParser commandParser = new CommandParser("java -jar example.jar");
         commandParser.setProgramDisplayName("Example")
                      .setVersion("1.0.0")
                      .setHelpDescription("A small example for the CommandParser library") 
                      .setHelpSynopsis("More information")
-                     .setHelpExample("java example -a --convert=true --group=1 -o output.txt");
+                     .setHelpExample("java -jar example.jar -a --convert=true --group=1 -o output.txt");
         commandParser.setPosArgsArity(0)       // Expects no positional arguments
                      .setPosArgsUsage("");     
 
